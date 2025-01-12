688b7a8b914355321d973d264de27d4424426ba7
Kevin Pangottil <kevinpangottil@Mac.attlocal.net>
2025-01-11 20:47:41 -0600

    Frontend and Backend Running

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 
 // MongoDB connection
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB Atas!');
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
