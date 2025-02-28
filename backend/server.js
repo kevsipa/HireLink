5858cc704d6925fd8e00ca64f296e0dced92e063
Kevin Pangottil <kevinpangottil@mac.lan>
2025-02-28 12:45:18 -0600

    Fixed port number

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/external-jobs', externalJobRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
